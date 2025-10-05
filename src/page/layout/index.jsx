b10485eccf910d6ff9d88faa4ab8843db5a6edde
wuxi <vcwx329@gmail.com>
2025-10-05 12:14:16 -0700

    modify default router month

--- src/page/layout/index.jsx
+++ src/page/layout/index.jsx
@@ -10,11 +10,13 @@ import {
   CalculatorOutline,
   AddCircleOutline,
 } from 'antd-mobile-icons'; // 从 antd-mobile-icons 导入图标
+import { useNavigate } from 'react-router-dom';
 
 // 底部导航栏的配置数据
 const tabs = [
   {
-    key: '/month',
+    //这里我们设置month为一级路由，因此key为/
+    key: '/',
     title: '月度账单',
     icon: <BillOutline />,
   },
@@ -25,7 +27,7 @@ const tabs = [
   },
   {
     key: '/year',
-    title: '/年度账单',
+    title: '年度账单',
     icon: <CalculatorOutline />,
   },
 ];
@@ -38,6 +40,11 @@ const Layout = () => {
     dispatch(getBillList());
   }, [dispatch]);
 
+  const navigate = useNavigate()
+  const switchRoute = (path) =>{
+    console.log(path)
+    navigate(path)
+  }
   return (
     <div className="layout">
       {/* 内容区域 */}
@@ -47,7 +54,8 @@ const Layout = () => {
 
       {/* 底部导航栏 */}
       <div className="footer">
-        <TabBar>
+        {/*点击 TabBar.Item 时，它的 key 自动作为参数传递给 onChange 函数——是 Ant Design Mobile (Antm) 这个库特有的设计机制。 */}
+        <TabBar onChange = {switchRoute}>
           {tabs.map(item => (
             <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
           ))}
